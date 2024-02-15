b4f4043d9fafa7866a6b6b095da08e7e8cc92b09
CaldeiraG <cal@caldeirag.xyz>
2024-02-15 16:22:53 +0000

    added some buttons for testing, adjusted css for light mode

--- components/ProfileButton.vue
+++ components/ProfileButton.vue
@@ -1,16 +1,46 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
-import { Mail } from 'lucide-vue-next'
+import { Mail, Github, Instagram, Twitter } from 'lucide-vue-next'
 </script>
 
 <template>
-  <div class="flex md:flex-grow flex-grow justify-center items-center pt-2 ">
-    <Button variant="destructive" class="bg-primary text-primary-foreground">
-      <Mail class="w-4 h-4 mr-2" /> Shoot me an email!
-    </Button>
+  <div class="container mx-auto">
+    <div class="grid grid-cols-1 gap-4 justify-center items-center pt-2">
+      <Button variant="default" class="bg-custom text-xl">
+        <Mail class="w-4 h-4 mr-2" /> Shoot me an email!
+      </Button>
+      <Button variant="default" class="bg-custom text-xl">
+        <github class="w-4 h-4 mr-2" /> GitHub
+      </Button>
+      <Button variant="default" class="bg-custom text-xl">
+        <instagram class="w-4 h-4 mr-2" /> Instagram
+      </Button>
+      <Button variant="default" class="bg-custom text-xl">
+        <twitter class="w-4 h-4 mr-2" /> Twitter
+      </Button>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style>
+
+.light-mode .bg-custom {
+  border-radius: 0.5em;
+  background-color: #333333;
+  color: #ffffff;
+}
+
+.dark-mode .bg-custom {
+  border-radius: 0.5em;
+  background-color: hsl(	215.4, 16.3%, 46.9%);
+  color: #e5e7eb;
+}
+
+.lucide {
+  /* Change this! */
+  width: 24px;
+  height: 24px;
+  stroke-width: 1px;
+}
 
 </style>
