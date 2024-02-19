a0dfe35335128bef7272c209f0f944d302074ecf
CaldeiraG <cal@caldeirag.xyz>
2024-02-19 14:36:38 +0000

    added placeholder pages so the build goes through hopefully

---
+++ components/Placeholder.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
+</script>
+
+<template>
+  <div class="flex md:flex-grow flex-grow justify-center items-center">
+    <h1>Test Page</h1>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/blog.vue
@@ -0,0 +1,90 @@
+<template>
+<div class="flex md:flex-grow flex-grow flex-row-reverse bg-primary text-primary-foreground">
+    <NavigationMenu class="justify-end">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NuxtLink to="/">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+              Home
+            </NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NuxtLink to="/blog">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+              About Me
+            </NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NuxtLink to="/portfolio">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+              Portfolio
+            </NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem class="p-2">
+          <select
+            v-model="colorMode.preference"
+            class="border w-24 h-8 bg-primary text-primary-foreground"
+          >
+            <option value="system">System</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+          </select>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </div>
+
+  <Placeholder/>
+</template>
+
+<style>
+
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: hsl(222.2 84% 4.9%);
+  color: hsl(210 40% 98%);
+}
+
+</style>
+
+<!--
+<script>
+
+export default {
+  head: {
+    title: 'My title',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'My description',
+      },
+    ],
+  },
+}
+
+</script>
+-->
+<script setup lang="ts">
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger, navigationMenuTriggerStyle
+} from '@/components/ui/navigation-menu'
+import ProfileImage from '~/components/ProfileImage.vue'
+import Placeholder from '~/components/Placeholder.vue'
+
+
+const colorMode = useColorMode()
+
+</script>
